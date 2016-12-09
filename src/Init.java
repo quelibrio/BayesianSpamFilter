e8041e3359cc78ee3cfdaec2013a9d52e05b90cf
Martin Ivanov <quelibrio@gmail.com>
2016-12-09 00:09:54 +0000

    FileInputStream reader added

--- src/Init.java
+++ src/Init.java
@@ -1,10 +1,14 @@
 import java.io.Console;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.Scanner;
 
 public class Init {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		File dir = new File("bin\\Mails");
 		System.out.println(dir.getAbsolutePath());
 		Scanner s = null;
@@ -13,9 +17,19 @@ public class Init {
 		for (File file : files) {
 			try{
 				s = new Scanner(file);
-			    String basePath = file.getPath();
-			    System.out.println(basePath);
-				System.out.println(file.length());
+				
+			    String path = file.getPath();
+			    Mail mail; 
+
+			    FileInputStream fis = new FileInputStream(file);
+			    byte[] data = new byte[(int) file.length()];
+			    fis.read(data);
+			    fis.close();
+			    String messageData = new String(data);
+		    	mail = new Mail(true, messageData);
+		    
+			    //System.out.println(basePath);
+				System.out.println(messageData);
 			}
 			catch(FileNotFoundException ex){
 				
