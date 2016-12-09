5821da5a46d30f1bdb1bf8394ba8a551ef1dad33
Martin Ivanov <quelibrio@gmail.com>
2016-12-09 01:13:36 +0000

    Mail class added

--- src/Init.java
+++ src/Init.java
@@ -5,39 +5,51 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Init {
 	public static void main(String[] args) throws IOException {
 		File dir = new File("bin\\Mails");
 		System.out.println(dir.getAbsolutePath());
-		Scanner s = null;
+	
 		File[] files = dir.listFiles();
 		if(dir.isDirectory()) {
 		for (File file : files) {
 			try{
-				s = new Scanner(file);
-				
-			    String path = file.getPath();
-			    Mail mail; 
-
-			    FileInputStream fis = new FileInputStream(file);
-			    byte[] data = new byte[(int) file.length()];
-			    fis.read(data);
-			    fis.close();
-			    String messageData = new String(data);
-		    	mail = new Mail(true, messageData);
-		    
-			    //System.out.println(basePath);
-				System.out.println(messageData);
+			    ArrayList<Mail> mails = new ArrayList<Mail>();
+		
+			    Mail parsedMail = GetMailFromFile(file);
+			    mails.add(parsedMail);
+					System.out.println(parsedMail.body);
+				}
+				catch(FileNotFoundException ex){
+					
+				}
+			    finally{
+			    	
+			    }
 			}
-			catch(FileNotFoundException ex){
-				
-			}
-		    finally{
-		    	s.close();
-		    }
-		}
 		}
 	}
+	
+	private static Mail GetMailFromFile(File file) throws IOException{
+	    String path = file.getPath();
+	    FileInputStream fis = new FileInputStream(file);
+	    byte[] data = new byte[(int) file.length()];
+	    fis.read(data);
+	    fis.close();
+	    
+	    Mail mail; 
+	    String messageData = new String(data);
+	    if(path.contains("spam")){
+	    	//Spam mail
+	    	mail = new Mail(true, messageData);
+	    }else{
+	    	//Ham mail
+	    	mail = new Mail(false, messageData);
+	    }
+	    return mail;
+	}
+	
 }
--- src/Mail.java
+++ src/Mail.java
@@ -5,5 +5,10 @@ public class Mail {
 	public Mail(boolean spam, String unformatted){
 		this.spam = spam;
 		this.body = unformatted;
+		//TODO: Try this http://stackoverflow.com/questions/3444660/java-email-message-parser
+//		String content = ...
+//				Session s = Session.getDefaultInstance(new Properties());
+//		InputStream is = new ByteArrayInputStream(content.getBytes());
+//		MimeMessage message = new MimeMessage(s, is);
 	}
 }
