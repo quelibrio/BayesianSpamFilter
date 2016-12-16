7f06afb1954ec9a7e03ce3589abbae9506bac7b5
Martin Ivanov <quelibrio@gmail.com>
2016-12-16 22:30:46 +0000

    Neutral words filtering removed

--- src/ModelValidations.java
+++ src/ModelValidations.java
@@ -95,7 +95,7 @@ public class ModelValidations {
 			}
 			else{
 				bayse.Train(testType,trainMails);
-				bayse.RemoveNeutralWords();
+				//bayse.RemoveNeutralWords();
 			}
 			TestResult result= new TestResult();
 			result.dataSize = testMails.size();
